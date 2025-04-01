a45a93d4ced3dfd53a775d7e64792a796fde6857
kyler <95313103+senyc@users.noreply.github.com>
2025-04-01 16:09:15 -0400

    feat: correct top bar "show completed" switch to include search params in redirect

--- subtasks/src/components/projects/ProjectBreadCrumbs.vue
+++ subtasks/src/components/projects/ProjectBreadCrumbs.vue
@@ -5,49 +5,81 @@
         <template #home-icon>
           <div />
         </template>
-        <RouterLink class="text-gray-700 hover:text-gray-900 dark:hover:text-white text-[17px]" to="/">
+        <RouterLink
+          class="text-gray-700 hover:text-gray-900 dark:hover:text-white text-[17px]"
+          to="/"
+        >
           Subtasks
         </RouterLink>
       </fwb-breadcrumb-item>
       <fwb-breadcrumb-item>
         <template #arrow-icon>
-          <svg class="mr-1 size-7 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd"
+          <svg
+            class="mr-1 size-7 text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              fill-rule="evenodd" />
+              fill-rule="evenodd"
+            />
           </svg>
         </template>
-        <RouterLink class="text-gray-700 hover:text-gray-900 dark:hover:text-white text-[17px]" to="/projects">Projects
+        <RouterLink
+          class="text-gray-700 hover:text-gray-900 dark:hover:text-white text-[17px]"
+          to="/projects"
+          >Projects
         </RouterLink>
       </fwb-breadcrumb-item>
       <fwb-breadcrumb-item class="text-xl" v-if="showProjectName">
-        <RouterLink class="text-gray-700 hover:text-gray-900 dark:hover:text-white text-[17px]"
-          :to="showProjectName ? `/projects/${projectId}/tasks` : '#'">{{ project?.title }}
+        <RouterLink
+          class="text-gray-700 hover:text-gray-900 dark:hover:text-white text-[17px]"
+          :to="showProjectName ? `/projects/${projectId}/tasks` : '#'"
+          >{{ project?.title }}
         </RouterLink>
         <template #arrow-icon>
-          <svg class="mr-1 size-7 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd"
+          <svg
+            class="mr-1 size-7 text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              fill-rule="evenodd" />
+              fill-rule="evenodd"
+            />
           </svg>
         </template>
       </fwb-breadcrumb-item>
       <fwb-breadcrumb-item class="text-xl" v-if="completed" href="#">
         <template #arrow-icon>
-          <svg class="mr-1 size-7 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd"
+          <svg
+            class="mr-1 size-7 text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              fill-rule="evenodd" />
+              fill-rule="evenodd"
+            />
           </svg>
         </template>
         <p class="text-[17px]">Completed</p>
       </fwb-breadcrumb-item>
     </fwb-breadcrumb>
     <div class="flex gap-5 items-center ml-auto">
-      <fwb-toggle :model-value="completed" @change="toggleIncomplete" reverse label="Show Completed" size="sm" />
+      <fwb-toggle
+        :model-value="completed"
+        @change="toggleIncomplete"
+        reverse
+        label="Show Completed"
+        size="sm"
+      />
       <div v-if="!showProjectName" class="ml-auto">
         <NewProject />
       </div>
@@ -59,33 +91,43 @@
 </template>
 
 <script setup lang="ts">
-import { FwbBreadcrumb, FwbBreadcrumbItem, FwbToggle } from 'flowbite-vue'
-import { useRoute, useRouter } from 'vue-router'
+import { FwbBreadcrumb, FwbBreadcrumbItem, FwbToggle } from "flowbite-vue";
+import { useRoute, useRouter } from "vue-router";
 
-import useProject from '../../composables/project'
-import NewProject from './NewProject.vue'
-import NewTask from '../NewTask.vue'
-const route = useRoute()
-const router = useRouter()
+import useProject from "../../composables/project";
+import NewProject from "./NewProject.vue";
+import NewTask from "../NewTask.vue";
+const route = useRoute();
+const router = useRouter();
 
-const props = withDefaults(defineProps<{
-  completed?: boolean
-  showProjectName?: boolean
-}>(), {
-  completed: false,
-  showProjectName: false
-})
+const props = withDefaults(
+  defineProps<{
+    completed?: boolean;
+    showProjectName?: boolean;
+  }>(),
+  {
+    completed: false,
+    showProjectName: false,
+  },
+);
 
-
-const projectId = parseInt(route.params.id as string || "")
-const { data: project } = useProject({ enabled: props.showProjectName, id: projectId })
+const projectId = parseInt((route.params.id as string) || "");
+const { data: project } = useProject({
+  enabled: props.showProjectName,
+  id: projectId,
+});
 
 function toggleIncomplete() {
   if (props.completed) {
-    router.back()
+    router.push({
+      path: route.path.replace("/completed", "/"),
+      query: route.query,
+    });
   } else {
-    router.push(`${route.path.replace(/\/$/, '')}/completed`)
+    router.push({
+      path: `${route.path.replace(/\/$/, "")}/completed`,
+      query: route.query,
+    });
   }
 }
-
 </script>
